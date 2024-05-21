b47978816f12defc2f78432abb7a871f70c623a1
NIhat <135535115+Nihatgunay@users.noreply.github.com>
2024-05-21 12:21:42 +0400

    Add files via upload

---
+++ valid passord.cs
@@ -0,0 +1,47 @@
+﻿namespace Password
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ValidatePassword("Sal1am2&"));
+
+        }
+
+        //TASK---1
+
+        static bool ValidatePassword(string password)
+        {
+            bool digit = false;
+            bool Character = false;
+
+            if (password.Length < 8)
+            {
+                return false;
+            }
+            else if (!char.IsUpper(password[0]))
+            {
+                return false;
+            }
+
+            foreach (char a in password)
+            {
+                if (char.IsDigit(a))
+                {
+                    digit = true;
+                }
+                else if (char.IsLetterOrDigit(a))
+                {
+                    Character = true;
+                }
+                else if (digit && Character)
+                {
+                    return true;
+                }
+
+            }
+            return false;
+
+        }
+    }
+}
